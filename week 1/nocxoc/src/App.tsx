4b6c6c7ca74df4344d06e84275a222d26efd89b8
Yeonjoo Lee <frisk0608@gmail.com>
2025-04-23 15:03:37 +0900

    card container & card

--- week 1/nocxoc/src/App.tsx
+++ week 1/nocxoc/src/App.tsx
@@ -8,26 +8,46 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='background'>
+        <div className='card-container'>
+          <div className='card'>
+            <p>
+              telegram
+            </p>
+            <img src={reactLogo} alt="logo" className="logo"/>
+          </div>
+          <div className='card'>
+            <p>
+              telegram
+            </p>
+            <img src={reactLogo} alt="logo" className="logo"/>
+          </div>
+          <div className='card'>
+            <p>
+              telegram
+            </p>
+            <img src={reactLogo} alt="logo" className="logo"/>
+          </div>
+          <div className='card'>
+            <p>
+              telegram
+            </p>
+            <img src={reactLogo} alt="logo" className="logo"/>
+          </div>
+          <div className='card'>
+            <p>
+              telegram
+            </p>
+            <img src={reactLogo} alt="logo" className="logo"/>
+          </div>
+          <div className='card'>
+            <p>
+              telegram
+            </p>
+            <img src={reactLogo} alt="logo" className="logo"/>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count * 2)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
